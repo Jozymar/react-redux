5be786edfdca1187f21ba958249d6316744ea978
Jozimar <jozimarsoares.js@gmail.com>
2019-11-04 21:50:20 -0300

    Conteúdo até a aula 59

---
+++ todo-app/frontend/src/about/about.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        {/* <h1>Sobre</h1> */}
+        <PageHeader name='Sobre' small='Nós'></PageHeader>
+
+        <h2>Nossa Hitória</h2>
+        <p>História...</p>
+        <h2>Missão e Visão</h2>
+        <p>Missão...</p>
+        <h2>Imprensa</h2>
+        <p>Imprensa...</p>
+    </div>
+)
